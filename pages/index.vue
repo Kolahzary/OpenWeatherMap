7581eabdbdb404d04fe2bbd801df990a53b62222
Shadman Kolahzary <shadman.ko@gmail.com>
2021-04-21 14:44:39 +0430

    feat: add data table ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,125 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
+    <v-data-table
+      :headers="headers"
+      :items="records"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
   </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Date',
+          align: 'start',
+          value: 'dt',
+        },
+        { text: 'City name', value: 'name' },
+        { text: 'Wind speed', value: 'wind.speed' },
+        { text: 'Humidity', value: 'main.humidity' },
+        { text: 'Pressure', value: 'main.pressure' },
+        { text: 'Temp', value: 'main.temp' },
+        { text: 'Min Temp', value: 'main.temp_min' },
+        { text: 'Max Temp', value: 'main.temp_max' },
+        { text: 'Lat', value: 'coord.lat' },
+        { text: 'Lon', value: 'coord.lon' },
+        { text: 'Description', value: 'weather[0].description' },
+      ],
+      // TODO: read records every 2 minutes from http://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=79c06f01f69f3f8a9cb88a7949c4a6d5&units=metric
+      records: [
+        {
+          coord: {
+            lon: 2.159,
+            lat: 41.3888,
+          },
+          weather: [
+            {
+              id: 803,
+              main: 'Clouds',
+              description: 'broken clouds',
+              icon: '04d',
+            },
+          ],
+          base: 'stations',
+          main: {
+            temp: 13.49,
+            feels_like: 12.77,
+            temp_min: 12.22,
+            temp_max: 14.44,
+            pressure: 1014,
+            humidity: 72,
+          },
+          visibility: 10000,
+          wind: {
+            speed: 4.12,
+            deg: 50,
+          },
+          clouds: {
+            all: 75,
+          },
+          dt: 1618998813,
+          sys: {
+            type: 1,
+            id: 6398,
+            country: 'ES',
+            sunrise: 1618981340,
+            sunset: 1619030243,
+          },
+          timezone: 7200,
+          id: 3128760,
+          name: 'Barcelona',
+          cod: 200,
+        },
+        {
+          coord: {
+            lon: -3.7026,
+            lat: 40.4165,
+          },
+          weather: [
+            {
+              id: 800,
+              main: 'Clear',
+              description: 'clear sky',
+              icon: '01d',
+            },
+          ],
+          base: 'stations',
+          main: {
+            temp: 287.99,
+            feels_like: 286.97,
+            temp_min: 287.04,
+            temp_max: 289.26,
+            pressure: 1012,
+            humidity: 55,
+          },
+          visibility: 10000,
+          wind: {
+            speed: 1.54,
+            deg: 100,
+          },
+          clouds: {
+            all: 0,
+          },
+          dt: 1618998601,
+          sys: {
+            type: 1,
+            id: 6443,
+            country: 'ES',
+            sunrise: 1618982837,
+            sunset: 1619031558,
+          },
+          timezone: 7200,
+          id: 3117735,
+          name: 'Madrid',
+          cod: 200,
+        },
+      ],
+    }
   },
 }
 </script>
